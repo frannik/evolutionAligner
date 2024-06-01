73a91e12273408db264852d9437b854e5ade95a5
Frannik <francisco.niklison@gmail.com>
2024-06-01 18:25:12 -0300

    metodos .stl y metodos Descarga de archivos

--- src/main/java/com/fran/ogascon/servicios/TratamientoServicio.java
+++ src/main/java/com/fran/ogascon/servicios/TratamientoServicio.java
@@ -14,14 +14,22 @@ import com.fran.ogascon.repositorios.TratamientoRepositorio;
 import com.fran.ogascon.repositorios.UsuarioRepositorio;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -112,84 +120,6 @@ public class TratamientoServicio {
                 imagenes.add(imagen);
             }
             t.setImagenesPaciente(imagenes);
-//             Imagen imagen = new Imagen();
-//                imagen.setMime(rxPanoramica.getContentType());
-//                imagen.setNombre(rxPanoramica.getName());
-//                imagen.setContenido(rxPanoramica.getBytes());
-//                 imagen.setTratamiento(t);
-
-            //VALIDAR Y MENSAJES DE NULL
-//             listado.add(imagen);
-////----------------------------------------------------
-//             List<Imagen> listado = new ArrayList();
-//             MultipartFile archivo = rxPanoramica;
-//             Imagen imagen = imagenServicio.guardar(archivo, t);
-//             listado.add(imagen);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo1 = lateralDer;
-//             Imagen imagen1 = imagenServicio.guardar(archivo1, t);
-//             listado.add(imagen1);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo2 = lateralIzq;
-//             Imagen imagen2 = imagenServicio.guardar(archivo2, t);
-//             listado.add(imagen2);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo3 = inoclusion;
-//             Imagen imagen3 = imagenServicio.guardar(archivo3, t);
-//             listado.add(imagen3);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo4 = oclusal;
-//             Imagen imagen4 = imagenServicio.guardar(archivo4, t);
-//             listado.add(imagen4);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo5 = oclusalSup;
-//             Imagen imagen5 = imagenServicio.guardar(archivo5, t);
-//             listado.add(imagen5);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo6 = oclusalInf;
-//             Imagen imagen6 = imagenServicio.guardar(archivo6, t);
-//             listado.add(imagen6);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo7 = teleradiografia;
-//             Imagen imagen7 = imagenServicio.guardar(archivo7, t);
-//             listado.add(imagen7);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo8 = topografiaConeBean;
-//             Imagen imagen8 = imagenServicio.guardar(archivo8, t);
-//             listado.add(imagen8);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo9 = imagenFrente;
-//             Imagen imagen9 = imagenServicio.guardar(archivo9, t);
-//             listado.add(imagen9);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo10 = imagenFrenteSonrisa;
-//             Imagen imagen10 = imagenServicio.guardar(archivo10, t);
-//             listado.add(imagen10);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo11 = imagenPerfil;
-//             Imagen imagen11 = imagenServicio.guardar(archivo11, t);
-//             listado.add(imagen11);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo12 = imagenPerfilConProyeccion;
-//             Imagen imagen12 = imagenServicio.guardar(archivo12, t);
-//             listado.add(imagen12);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo13 = videoMotivoDeConsulta;
-//             Imagen imagen13 = imagenServicio.guardar(archivo13, t);
-//             listado.add(imagen13);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo14 = estudioMcNamara;
-//             Imagen imagen14 = imagenServicio.guardar(archivo14, t);
-//             listado.add(imagen14);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo15 = estudioRickets;
-//             Imagen imagen15 = imagenServicio.guardar(archivo15, t);
-//             listado.add(imagen15);
-//             t.setImagenesPaciente(listado);
-//             MultipartFile archivo16 = odontograma;
-//             Imagen imagen16 = imagenServicio.guardar(archivo16, t);
-//             listado.add(imagen16);
-//             t.setImagenesPaciente(listado);
             t.setPrioridadEsteticaPaciente(prioridadEsteticaPaciente);
             t.setCorregirLineaMedia(corregirLineaMedia);
             t.setLineaMediaSup(lineaMediaSup);
@@ -224,6 +154,33 @@ public class TratamientoServicio {
             // Manejar el caso en el que no se encuentre el tratamiento
             return null; // o lanzar una excepción, dependiendo de tus requerimientos
         }
+    }
+    
+    // Método para obtener un archivo específico
+    public Resource obtenerArchivo(Imagen imagen) throws IOException {
+        File file = new File(imagen.getId());  // VERIFICAR SI ESTA BIEN EL getId() 
+        return new InputStreamResource(new FileInputStream(file));
+    }
+
+    // Método para obtener todos los archivos y comprimirlos en un ZIP
+    public Resource obtenerArchivosComoZip(List<Imagen> imagenes) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(byteArrayOutputStream)) {
+            for (Imagen imagen : imagenes) {
+                File file = new File(imagen.getId());
+                try (FileInputStream fileInputStream = new FileInputStream(file)) {
+                    ZipEntry zipEntry = new ZipEntry(file.getName());
+                    zipOutputStream.putNextEntry(zipEntry);
+                    byte[] bytes = new byte[1024];
+                    int length;
+                    while ((length = fileInputStream.read(bytes)) >= 0) {
+                        zipOutputStream.write(bytes, 0, length);
+                    }
+                    zipOutputStream.closeEntry();
+                }
+            }
+        }
+        return new InputStreamResource(new ByteArrayInputStream(byteArrayOutputStream.toByteArray()));
     }
     
     private void validarTratamiento(String idPaciente, String idUsuario, Patologia patologia, TipoTratamiento tipoTratamiento,
